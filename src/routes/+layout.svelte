424696a2b85306a0cc08c7f35ee9cd7472d1f948
botlane-feeder <tserieye.pro@gmail.com>
2024-12-26 02:41:20 +0100

    UPDATE : Mise en place de la page générale et intégration des composants

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import 'bootstrap/dist/css/bootstrap.min.css';
+
+  let { children } = $props();
+
+</script>
+
+<style>
+  div{
+    margin: 20px;
+  }
+</style>
+
+<div>
+  {@render children()}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,45 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+
+  import GlobalInfo from "../components/GlobalInfo.svelte"
+  import RecipeComposition from "../components/RecipeComposition.svelte"
+  import ResumeSummary from "../components/ResumeSummary.svelte"
+
+  const allData:any={
+    "maintenanceEnergyNeededFactor":{"dog":130, "cat":100}
+  }
+
+  let species:string = $state("cat");
+  let currentWeight:number = $state(5);
+  let bodyConditionScore:number = $state(3); //NEC
+  let idealWeight:number = $derived( Math.round(currentWeight * ( 1 - ( bodyConditionScore - 5)/10 )*100)/100 );
+  let appliedEnergyNeeded:number = $state(360);
+  let growingActivityPower:number = $state(1);
+  let maintenanceEnergyNeeded:number = $derived( allData["maintenanceEnergyNeededFactor"][species] * idealWeight * growingActivityPower);
+
+
+</script>
+
+<style>
+  h1{
+    text-align: center;
+  }
+</style>
+
+<div class="row">
+  <h1>Composition d'une ration ménagère</h1>
+  <div class="col-3">
+    <GlobalInfo 
+      bind:species={species}
+      bind:currentWeight={currentWeight}
+      bind:bodyConditionScore={bodyConditionScore}
+      idealWeight={idealWeight}
+      bind:appliedEnergyNeeded={appliedEnergyNeeded}
+      bind:growingActivityPower={growingActivityPower}
+      maintenanceEnergyNeeded={maintenanceEnergyNeeded}
+    />
+  </div>
+  <div class="col-9">
+    <RecipeComposition />
+    <ResumeSummary />
+  </div>
+</div>
