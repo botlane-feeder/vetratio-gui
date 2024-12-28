bfece2314ba43a045fae90b7cd765be623282957
botlane-feeder <tserieye.pro@gmail.com>
2024-12-28 19:44:21 +0100

    ADD : Ajout d'une librairie de type

---
+++ src/lib/types.ts
@@ -0,0 +1,2 @@
+export type FoodCompositionType = {name:string; quantity:number; protein:number;};
+// export type FoodCompositionType = {name:string; quantity:number; protein:number; glucide:number; calcium:number; phosphore:number;};
