0fd1a14e2ccf9e2ec214cf7123e8f7fabc93c5fb
botlane-feeder <tserieye.pro@gmail.com>
2024-12-28 19:44:53 +0100

    UPDATE : Simplification du la lib/index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+//Fonctions mathématiques
 export function roundTwo(toRound:number):number{ 
   return Math.round(toRound*100)/100;
 }
